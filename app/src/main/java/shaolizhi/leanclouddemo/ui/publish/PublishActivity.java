bc0b0f7724defa77acd4d5ce52bc204d3fc1b59a
shaolizhi1234 <shaolizhi1234@126.com>
2018-02-05 17:09:25 +0800

    PublishActivity：提交功能

--- app/src/main/java/shaolizhi/leanclouddemo/ui/publish/PublishActivity.java
+++ app/src/main/java/shaolizhi/leanclouddemo/ui/publish/PublishActivity.java
@@ -1,14 +1,23 @@
 package shaolizhi.leanclouddemo.ui.publish;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.ActionBar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVFile;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVUser;
+import com.avos.avoscloud.SaveCallback;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -21,18 +30,88 @@ public class PublishActivity extends BaseActivity {
 
     private TextInputEditText titleEditText;
     private TextInputEditText descriptionEditText;
-    private TextInputEditText moneyEditText;
+    private TextInputEditText priceEditText;
     private Button imageSelectButton;
     private Button commitButton;
     private ImageView imageView;
 
     private final int REQUEST_CODE_IMAGE_SELECT = 42;
 
-    private byte[] selctedImageData;
+    private byte[] selectedImageData;
 
     //commit button click event
     private void clickCommitButton() {
+        attemptCommit();
+    }
 
+    private void attemptCommit() {
+
+        final String title = titleEditText.getText().toString();
+        final String description = descriptionEditText.getText().toString();
+        final String price = priceEditText.getText().toString();
+
+        if (checkInput(title, description, price)) {
+            //do something
+            final ProgressDialog dialog = ProgressDialog.show(this, "提示", "正在提交中");
+            AVObject product = new AVObject("Product");
+            product.put("title", title);
+            product.put("description", description);
+            product.put("price", price);
+            product.put("owner", AVUser.getCurrentUser());
+            product.put("image", new AVFile("productPic", selectedImageData));
+            product.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(AVException e) {
+                    if (e == null) {
+                        dialog.dismiss();
+                        PublishActivity.this.finish();
+                    } else {
+                        dialog.dismiss();
+                        Toast.makeText(PublishActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private Boolean checkInput(String title, String description, String price) {
+        titleEditText.setError(null);
+        descriptionEditText.setError(null);
+        priceEditText.setError(null);
+
+
+        Boolean isInputMistake = false;
+        TextInputEditText inputMistakeEditText = null;
+
+        if (TextUtils.isEmpty(price)) {
+            priceEditText.setError(getString(R.string.publish_act_string10));
+            inputMistakeEditText = priceEditText;
+            isInputMistake = true;
+        }
+
+        if (TextUtils.isEmpty(description)) {
+            descriptionEditText.setError(getString(R.string.publish_act_string9));
+            inputMistakeEditText = descriptionEditText;
+            isInputMistake = true;
+        }
+
+        if (TextUtils.isEmpty(title)) {
+            titleEditText.setError(getString(R.string.publish_act_string8));
+            inputMistakeEditText = titleEditText;
+            isInputMistake = true;
+        }
+
+
+        if (isInputMistake) {
+            inputMistakeEditText.requestFocus();
+        } else {
+            if (selectedImageData == null) {
+                Toast.makeText(PublishActivity.this, R.string.publish_act_string11, Toast.LENGTH_SHORT).show();
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 
     //image select button click event
@@ -72,7 +151,7 @@ public class PublishActivity extends BaseActivity {
     private void initView() {
         titleEditText = findViewById(R.id.publish_act_textinputedittext1);
         descriptionEditText = findViewById(R.id.publish_act_textinputedittext2);
-        moneyEditText = findViewById(R.id.publish_act_textinputedittext3);
+        priceEditText = findViewById(R.id.publish_act_textinputedittext3);
         imageSelectButton = findViewById(R.id.publish_act_button1);
         commitButton = findViewById(R.id.publish_act_button2);
         imageView = findViewById(R.id.publish_act_imageview1);
@@ -101,7 +180,7 @@ public class PublishActivity extends BaseActivity {
             try {
                 if (data.getData() != null) {
                     imageView.setImageBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData()));
-                    selctedImageData = getBytes(getContentResolver().openInputStream(data.getData()));
+                    selectedImageData = getBytes(getContentResolver().openInputStream(data.getData()));
                 }
             } catch (IOException e) {
                 e.printStackTrace();
