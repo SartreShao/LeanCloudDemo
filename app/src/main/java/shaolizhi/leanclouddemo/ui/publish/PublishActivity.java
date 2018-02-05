64f4d402672cf3005d400a041d7c905abc58c78f
shaolizhi1234 <shaolizhi1234@126.com>
2018-02-05 16:28:03 +0800

    PublishActivity：从相册中选择图片功能

--- app/src/main/java/shaolizhi/leanclouddemo/ui/publish/PublishActivity.java
+++ app/src/main/java/shaolizhi/leanclouddemo/ui/publish/PublishActivity.java
@@ -3,12 +3,44 @@ package shaolizhi.leanclouddemo.ui.publish;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import shaolizhi.leanclouddemo.R;
 import shaolizhi.leanclouddemo.ui.base.BaseActivity;
 
 public class PublishActivity extends BaseActivity {
+
+    private TextInputEditText titleEditText;
+    private TextInputEditText descriptionEditText;
+    private TextInputEditText moneyEditText;
+    private Button imageSelectButton;
+    private Button commitButton;
+    private ImageView imageView;
+
+    private final int REQUEST_CODE_IMAGE_SELECT = 42;
+
+    private byte[] selctedImageData;
+
+    //commit button click event
+    private void clickCommitButton() {
+
+    }
+
+    //image select button click event
+    private void clickImageSelectButton() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_CODE_IMAGE_SELECT);
+    }
 
     @Override
     protected int layoutId() {
@@ -18,6 +50,32 @@ public class PublishActivity extends BaseActivity {
     @Override
     protected void created(Bundle bundle) {
         setActionBarTitle();
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        imageSelectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickImageSelectButton();
+            }
+        });
+        commitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickCommitButton();
+            }
+        });
+    }
+
+    private void initView() {
+        titleEditText = findViewById(R.id.publish_act_textinputedittext1);
+        descriptionEditText = findViewById(R.id.publish_act_textinputedittext2);
+        moneyEditText = findViewById(R.id.publish_act_textinputedittext3);
+        imageSelectButton = findViewById(R.id.publish_act_button1);
+        commitButton = findViewById(R.id.publish_act_button2);
+        imageView = findViewById(R.id.publish_act_imageview1);
     }
 
     private void setActionBarTitle() {
@@ -34,5 +92,31 @@ public class PublishActivity extends BaseActivity {
 
     public static Intent newIntent(Context packageContext) {
         return new Intent(packageContext, PublishActivity.class);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_IMAGE_SELECT && resultCode == RESULT_OK) {
+            try {
+                if (data.getData() != null) {
+                    imageView.setImageBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData()));
+                    selctedImageData = getBytes(getContentResolver().openInputStream(data.getData()));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public byte[] getBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+        int len;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteArrayOutputStream.write(buffer, 0, len);
+        }
+        return byteArrayOutputStream.toByteArray();
     }
 }
